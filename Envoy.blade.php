a960f1c6e40dbdfc958428d7516d331679bb0fbc
RevCrew <dimaelik71@gmail.com>
2023-02-17 21:15:53 +0300

    fix

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -8,9 +8,11 @@
 @task('deploy', ['on' => 'production'])
 cd {{ env('DEPLOY_PATH') }}
 git pull
-make install_prod
-make artisan_prod migrate --force
-make artisan_prod optimize
+docker-compose -f docker-compose.prod.yml exec web composer install --ansi --prefer-dist
+{{--make artisan_prod "migrate --force"--}}
+docker-compose -f docker-compose.prod.yml exec web "php artisan migrate --force"
+docker-compose -f docker-compose.prod.yml exec web php artisan optimize
+make ps
 @endtask
 
 @task('deploy_with_docker', ['on' => 'production'])
