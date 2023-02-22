93dbff55e798af2903d1de4ea3108114c8635752
RevCrew <dimaelik71@gmail.com>
2023-02-23 00:47:00 +0300

    add user & auth

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,10 +28,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyTwoMinutes();
 
         $schedule->command(DispatchQueueCheckJobsCommand::class)->everyMinute();
         $schedule->command(ScheduleCheckHeartbeatCommand::class)->everyMinute();
+
+        $schedule->command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyTwoMinutes();
     }
 
     /**
