4d20a1a1b7f1bf574f33212715f0508440497f20
RevCrew <dimaelik71@gmail.com>
2023-02-23 13:18:25 +0300

    add user & auth

---
+++ Modules/Authorization/Resources/lang/ru/authorization.php
@@ -0,0 +1,47 @@
+<?php
+
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'activation_email_subject' => 'Активация аккаунта Katawa-Suite',
+    'reset_password_subject' => 'Запрос на сброс пароля',
+
+    'password' => 'Некорректный пароль',
+    'failed' => 'Неверные учетные данные.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте еще раз через :seconds секунд.',
+
+    'active' => 'Данный аккаунт не активирован, пожалуйста перейдите по ссылке в письме.',
+
+    'verifyEmail' => 'Подтвердить email',
+    'verifyEmailAddress' => 'Пожалуйста, подтвердите ваш email адрес',
+    'verifyEmailIgnore' => 'Если вы получили это письмо по ошибке, то проигнорируйте его',
+    'verifyEmailLink' => 'Чтобы подтвердить ваш email адрес перейдите по ссылке ниже',
+
+    'resetPassword' => 'Сброс пароля',
+    'resetPasswordEmailIgnore' => 'Если вы получили это письмо по ошибке, то проигнорируйте его',
+
+    'resetPasswordNew' => 'Новый пароль',
+    'resetPasswordNewConfirm' => 'Повторите пароль',
+
+    'emailAlreadyExist' => 'Данный email уже используется',
+    'oldPasswordConfirmation' => 'Неправильно введен старый пароль',
+
+    'temporaryDisabled' => 'Регистрация временно отключена, пишите в сообщения группы https://vk.com/katawa.suite',
+
+    'provider_google' => 'Google',
+    'provider_vk' => 'VK',
+    'provider_telegram' => 'Telegram',
+
+    'oauth_v2' => 'Поддерживается только вторая версия OAuth',
+    'oauth_empty_email' => 'Не удалось авторизоровать пользователя без email.'
+];
---
+++ Modules/User/Services/UserSocialService.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Modules\User\Services;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Modules\User\Entities\DTO\RegisterSocialDto;
+use Modules\User\Entities\UserSocial;
+use Modules\User\Enums\AuthProviderEnum;
+
+class UserSocialService implements UserSocialServiceInterface
+{
+    public function canUseNameAttribute(AuthProviderEnum $authProvider): bool
+    {
+        foreach (AuthProviderEnum::withNameAttribute() as $provider) {
+            if ($provider->value === $authProvider->value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getSocialsByUser(User $user): EloquentCollection
+    {
+        return UserSocial::whereUserId($user->id)->get();
+    }
+
+    public function findByProvider(AuthProviderEnum $provider, int $providerUserId): ?UserSocial
+    {
+        return UserSocial::query()
+            ->where([
+                'provider' => $provider->value,
+                'provider_id' => $providerUserId,
+            ])
+            ->first();
+    }
+
+    public function create(RegisterSocialDto $dto): UserSocial
+    {
+        return UserSocial::create($dto->toArray());
+    }
+
+    public function changeAvatar(UserSocial $userSocial, string $avatar): bool
+    {
+        return $userSocial->fill(['avatar' => $avatar])->save();
+    }
+}
