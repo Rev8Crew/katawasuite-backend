5a082dea0318882f0206324f83fda6e8c2430075
RevCrew <dimaelik71@gmail.com>
2023-02-27 17:19:20 +0300

    add other modules

--- app/Http/Middleware/AdminEmailMiddleware.php
+++ app/Http/Middleware/AdminEmailMiddleware.php
@@ -4,15 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class AdminEmailMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        $user = $request->user();
+        $user = Auth::user();
 
-        if (! $user || in_array($user->email, config('app.admin_emails'), true)) {
+        if (!$user || in_array($user->email, config('app.admin_emails'), true)) {
             abort(SymfonyResponse::HTTP_FORBIDDEN);
         }
 
