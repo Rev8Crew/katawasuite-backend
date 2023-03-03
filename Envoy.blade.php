bd30b4a912484bb90b1ee1342dd8daefa433daf7
RevCrew <dimaelik71@gmail.com>
2023-03-03 18:38:19 +0300

    add other modules

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -14,6 +14,8 @@ make command_prod 'composer i && php artisan migrate --force && php artisan opti
 @task('deploy_docker', ['on' => 'production'])
 cd {{ env('DEPLOY_PATH') }}
 docker-compose -f docker-compose.prod.yml restart web
+docker-compose -f docker-compose.prod.yml restart queue
+docker-compose -f docker-compose.prod.yml restart cron
 @endtask
 
 @task('deploy_build', ['on' => 'production'])
