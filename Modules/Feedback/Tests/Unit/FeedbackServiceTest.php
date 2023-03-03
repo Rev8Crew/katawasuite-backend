e0aa76fb2e56f55771401d47a296debec077bb7d
alexkuzevich <enaken0212@gmail.com>
2023-03-03 18:51:46 +0300

    Feedback form test added

---
+++ Modules/Feedback/Tests/Unit/FeedbackServiceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Modules\Feedback\Tests\Unit;
+
+use AllowDynamicProperties;
+use Encore\Admin\Show\Relation;
+use Modules\Feedback\Entities\DTO\FeedbackCreateDto;
+use Modules\Feedback\Enums\FeedbackRelationEnum;
+use Modules\Feedback\Services\FeedbackServiceInterface;
+use Modules\User\Database\factories\UserFactory;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+#[AllowDynamicProperties] class FeedbackServiceTest extends TestCase
+{
+    private ?FeedbackServiceInterface $feedbackService = null;
+
+    private ?UserFactory $userFactory = null;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->feedbackService = $this->app->make(FeedbackServiceInterface::class);
+        $this->userFactory = $this->app->make(UserFactory::class);
+    }
+
+    public function testCreate(): void
+    {
+        $user = $this->userFactory->create();
+
+        $dto = new FeedbackCreateDto(
+            $this->faker->name(),
+            $this->faker->email(),
+            $this->faker->text(),
+            $user->id,
+            $this->relation = 'site'
+        );
+        //dd($dto);
+        $feedback = $this->feedbackService->create($dto);
+
+        //dd($feedback);
+        $this->assertSame($dto->name, $feedback->name);
+        $this->assertSame($dto->email, $feedback->email);
+        $this->assertSame($dto->text, $feedback->text);
+        $this->assertSame($dto->userId, $feedback->user_id);
+        $this->assertSame($dto->relation, $feedback->relation);
+    }
+}
