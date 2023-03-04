c6f2cce2a1e4990ba959e69c00f8ef68ca694c97
RevCrew <dimaelik71@gmail.com>
2023-03-04 12:19:28 +0300

    add other modules

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -16,6 +16,7 @@ cd {{ env('DEPLOY_PATH') }}
 docker-compose -f docker-compose.prod.yml restart web
 docker-compose -f docker-compose.prod.yml restart queue
 docker-compose -f docker-compose.prod.yml restart cron
+docker-compose -f docker-compose.prod.yml restart horizon
 @endtask
 
 @task('deploy_build', ['on' => 'production'])
