a051c9a969337334b7eeaa2e9fb9634e40aebe86
alexkuzevich <enaken0212@gmail.com>
2023-03-25 15:14:57 +0300

    telephone number change feature

---
+++ Modules/User/Http/Requests/ChangePhoneRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\User\Http\Requests;
+
+use App\Http\Requests\CustomFormRequest;
+
+class ChangePhoneRequest extends CustomFormRequest
+{
+
+    public function rules(): array
+    {
+        return [
+          'phone' => 'required|unique:users|phone:RU,UA,KZ',
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
