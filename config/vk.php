c72007bd9b79517f7344ed5c1ae52a8ad25f3ba4
RevCrew <dimaelik71@gmail.com>
2023-03-29 23:49:41 +0300

    fix

---
+++ config/vk.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'wall_id' => env('VK_GROUP_ID'),
+    'access_token' => env('VK_ACCESS_TOKEN'),
+
+    /**
+     *  https://vk.com/katawa.suite
+     */
+    'domain' => 'katawa.suite'
+];
