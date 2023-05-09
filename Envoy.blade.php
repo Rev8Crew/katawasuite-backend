609165cf163501b35400cc7e8c7d2e7329835d60
RevCrew <dimaelik71@gmail.com>
2023-05-09 11:31:08 +0300

    sentry

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -10,16 +10,6 @@ cd {{ env('DEPLOY_PATH') }}
 export SENTRY_DEPLOY_START=$(date +%s);
 git pull
 make command_prod 'composer i && php artisan migrate --force && php artisan optimize'
-export SENTRY_RELEASE_VERSION=$(git --git-dir .git log --pretty="%h" -n1 HEAD)
-export SENTRY_URL="{{ env('DEPLOY_SENTRY_URL') }}"
-export SENTRY_AUTH_TOKEN="{{ env('DEPLOY_SENTRY_TOKEN') }}"
-export SENTRY_ORG="{{ env('DEPLOY_SENTRY_ORG') }}"
-export SENTRY_PROJECT="{{ env('DEPLOY_SENTRY_PROJECT') }}"
-export ENVIRONMENT="{{ env('APP_ENV') }}"
-export SENTRY_DEPLOY_END=$(date +%s);
-sentry-cli --url $SENTRY_URL --auth-token $SENTRY_AUTH_TOKEN releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $SENTRY_RELEASE_VERSION;
-sentry-cli --url $SENTRY_URL --auth-token $SENTRY_AUTH_TOKEN releases -o $SENTRY_ORG set-commits --auto $SENTRY_RELEASE_VERSION;
-sentry-cli --url $SENTRY_URL --auth-token $SENTRY_AUTH_TOKEN deploys new -o $SENTRY_ORG -p $SENTRY_PROJECT -r $SENTRY_RELEASE_VERSION -e $ENVIRONMENT -n "$SENTRY_RELEASE_NAME" -t $((SENTRY_DEPLOY_END-SENTRY_DEPLOY_START));
 @endtask
 
 @task('deploy_docker', ['on' => 'production'])
